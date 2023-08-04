61376611dad37398ddb521d7ca00bafb2e10ed8d
Selly Djap <138817097+sd3219@users.noreply.github.com>
2023-08-04 15:20:59 -0400

    Update unemployment.py

--- app/unemployment.py
+++ app/unemployment.py
@@ -2,19 +2,6 @@
 #
 #API_KEY = getpass("Please input your AlphaVantage API Key: ")
 
-import os
-from dotenv import load_dotenv
-import requests
-import json
-from pprint import pprint
-from statistics import mean
-from plotly.express import line
-
-
-load_dotenv() #> invoking this function loads contents of the ".env" file into the script's environment...
-
-API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
-
 from app.alpha import API_KEY
 
 
