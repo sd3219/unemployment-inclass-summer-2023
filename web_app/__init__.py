9a1aa78251b1fdf390652db3b1ec02e48e6e999a
Selly Djap <138817097+sd3219@users.noreply.github.com>
2023-08-04 15:56:42 -0400

    Flash messages

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,5 +1,6 @@
 
 # this is the "web_app/__init__.py" file...
+import os
 
 from flask import Flask
 
@@ -9,8 +10,11 @@ from web_app.routes.stocks_routes import stocks_routes
 #from web_app.routes.book_routes import book_routes
 #from web_app.routes.weather_routes import weather_routes
 
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
+
 def create_app():
     app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY
     app.register_blueprint(home_routes)
     app.register_blueprint(unemployment_routes)
     app.register_blueprint(stocks_routes)
